725fa70f5e4914234877e87009f33bdaf9d517b2
Alvaro Barrado Fernandez <barradoa3648@gmail.com>
2019-05-12 16:14:25 +0200

    Dibujos de estrellas añadidos

--- levels/youngest/youngestMap.php
+++ levels/youngest/youngestMap.php
@@ -14,6 +14,10 @@ $image = $_SESSION['avatar'];
   <body style="background-color:lightgreen;overflow-y:initial;margin:0%;padding:0%;z-index:5;">
     <img src="../../Granja/GranjaFull.png" style="width:100%;height:100%;position:absolute;">
     <div class="primer" onclick="openGame()">
+      <img src="../../images/Identificar/0estrella.png" width="150px" style="position:absolute;top:0;display:none;" id="1s0"alt="">
+      <img src="../../images/Identificar/1estrella.png" width="150px" style="position:absolute;top:0;display:none;" id="1s1"alt="">
+      <img src="../../images/Identificar/2estrellas.png" width="150px" style="position:absolute;top:0;display:none;" id="1s2"alt="">
+      <img src="../../images/Identificar/3estrellas.png" width="150px" style="position:absolute;top:0;display:none;" id="1s3"alt="">
     </div>
     <dialog id="dialog" style="width:90%;height:90%;top:3%;z-index:1;"value="0">
       <iframe src="../../cowPuzzle.php" width="-webkit-fill-available" height="-webkit-fill-available" style="border:none;width:-webkit-fill-available;height:-webkit-fill-available;"id="frame1"></iframe>
@@ -22,25 +26,56 @@ $image = $_SESSION['avatar'];
       function boton() {
         document.getElementById('frame1').src = "../../pruebaIdentificar.php";
         var x = document.getElementById('frame1').contentWindow.document.getElementById('valor').value
-        if (x == 4) {
-          //window.alert(x);
+
+        if (document.getElementById('frame1').src = "../../pruebaIdentificar.php") {
           document.getElementById('boton').style.display = "none";
+          document.getElementById('boton2').style.display = "inline-block";
         }
+        if (document.getElementById('frame1').src = "../../carrotPuzzle.php") {
+
+        }
+        if (x == 4) {
+          document.getElementById('1s1').style.display = "inline-block";
+          window.alert(x);
+          document.getElementById('boton').style.display = "none";
+          document.getElementById('boton2').style.display = "inline-block";
+
+        }
+      }
+      function boton2() {
+        document.getElementById('frame1').src = "../../carrotPuzzle.php";
+        document.getElementById('boton2').style.display = "none";
+        document.getElementById('boton3').style.display = "inline-block";
+      }
+      function boton3(){
+        closeDialog();
       }
       </script>
       <button type="button" name="button" onclick="boton()" id="boton" class="botonN"style="display:inline-block;">Siguiente</button>
+      <button type="button" name="button" onclick="boton2()" id="boton2" class="botonN"style="display:none;">Siguiente</button>
+      <button type="button" name="button" onclick="boton3()" id="boton3" class="botonN"style="display:none;">Fin</button>
+
       <button type="button" name="button" onclick="closeDialog()"style="display:inline-block;position:absolute;top:0;left:0;margin:5px;">Cerrar</button>
 <img src="../../images/Cerrar.png" style="display:inline-block;position:absolute;top:0;left:0;margin:5px;" onclick="closeDialog()"alt="">
     </dialog>
 
     <div class="segundo">
-
+      <img src="../../images/Identificar/0estrella.png" width="150px" style="position:absolute;top:0;display:none;"alt="">
+      <img src="../../images/Identificar/1estrella.png" width="150px" style="position:absolute;top:0;display:none;"alt="">
+      <img src="../../images/Identificar/2estrellas.png" width="150px" style="position:absolute;top:0;display:none;"alt="">
+      <img src="../../images/Identificar/3estrellas.png" width="150px" style="position:absolute;top:0;display:none;"alt="">
     </div>
     <div class="tercero">
-
+      <img src="../../images/Identificar/0estrella.png" width="150px" style="position:absolute;top:0;display:none;"alt="">
+      <img src="../../images/Identificar/1estrella.png" width="150px" style="position:absolute;top:0;display:none;"alt="">
+      <img src="../../images/Identificar/2estrellas.png" width="150px" style="position:absolute;top:0;display:none;"alt="">
+      <img src="../../images/Identificar/3estrellas.png" width="150px" style="position:absolute;top:0;display:none;"alt="">
     </div>
     <div class="cuarto">
-
+      <img src="../../images/Identificar/0estrella.png" width="150px" style="position:absolute;top:0;display:none;"alt="">
+      <img src="../../images/Identificar/1estrella.png" width="150px" style="position:absolute;top:0;display:none;"alt="">
+      <img src="../../images/Identificar/2estrellas.png" width="150px" style="position:absolute;top:0;display:none;"alt="">
+      <img src="../../images/Identificar/3estrellas.png" width="150px" style="position:absolute;top:0;display:none;"alt="">
     </div>
     <!-- <img src="../../Granja/GranjaFull.png" style="width:100%;height:100%;position:absolute;" usemap="#mapa1"> -->
 
