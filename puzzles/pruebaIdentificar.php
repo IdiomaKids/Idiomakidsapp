44b7ab2e3b9ff83af6b9d7ac522220bfdfa056c0
Alvaro Barrado Fernandez <barradoa3648@gmail.com>
2019-06-07 18:30:35 +0200

    cuadrado

--- puzzles/pruebaIdentificar.php
+++ puzzles/pruebaIdentificar.php
@@ -17,7 +17,7 @@ $jsonP=$results;
 
  <audio id="win" src="https://raw.githubusercontent.com/IdiomaKids/Idiomakidsapp/rescate/clap2.mp3"></audio>
    <audio id="lose" src="https://raw.githubusercontent.com/IdiomaKids/Idiomakidsapp/rescate/lose.mp3"></audio>
-   <audio src="">
+   <audio id="square2" src="https://raw.githubusercontent.com/IdiomaKids/Idiomakidsapp/rescate/cuadrado.mp3">
 
    </audio>
   <img src="../images/Identificar/Cuadrado/square.png" alt="" class="fondo" id="fondoCorrect" solution="correct">
@@ -83,6 +83,7 @@ $jsonP=$results;
       // window.alert(document.getElementById('contador').value);
       document.getElementById("no").id="yes";
       document.getElementById("fondoCorrect").style.opacity = 1;
+      square2.play();
       win.play();
       document.getElementById("n").id= "voluble";
       setTimeout(function(){ document.getElementById("voluble").id = "n"; }, 3000);
